6d128ecc5aa9a511d1bcf2ad53b2755a0f844963
Allison <allisonhere>
2025-04-10 14:00:35 -0400

    some css updates and blog setup...wip

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const { DateTime} = require("luxon");
+
 module.exports = function(eleventyConfig){
 eleventyConfig.addPassthroughCopy("./src/styles.css");
 eleventyConfig.addPassthroughCopy("./src/assets");
+eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+eleventyConfig.addFilter("postDate", (dateObj) => {
+  return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+})
+
   return {
     dir: {
         input: "src",
