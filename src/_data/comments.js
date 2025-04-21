c33e627bc13665aafb010a52252bca725ceaf8a2
Allison <allisonhere>
2025-04-21 12:32:33 -0400

    config comments code

--- src/_data/comments.js
+++ src/_data/comments.js
@@ -1,31 +1,52 @@
-// // massage the approved comments data into the shape we'd like
-// // embelish the data with gravatars
 
-// const gravatar = require('gravatar');
-// const submissions = require('./approved-comments_submissions.json');
+// massage the approved comments data into the shape we'd like
+// embelish the data with gravatars
+
+const gravatar = require('gravatar');
+const submissions = require('./approved-comments_submissions.json');
 
 
-// module.exports = () => {
+module.exports = () => {
 
-//   let comments = {};
+  let comments = [];
 
-//   submissions.forEach(entry => {
-//     let comment = {
-//       name: entry.data.name,
-//       avatar: gravatar.url(entry.data.email, {s: '100', r: 'x', d: 'retro'}, true),
-//       comment: entry.data.comment.trim(),
-//       date: entry.data.received
-//     };
+  submissions.forEach(entry => {
+    let comment = {
+      name: entry.data.name,
+      avatar: gravatar.url(entry.data.email, {s: '100', r: 'x', d: 'retro'}, true),
+      comment: entry.data.comment.trim(),
+      date: entry.data.received
+    };
 
-//     // Add it to an existing array or create a new one in the comments object
-//     if(comments[entry.data.path]){
-//       comments[entry.data.path].push(comment);
-//     } else {
-//       comments[entry.data.path] = [comment];
-//     }
+    // Add it to an existing array or create a new one in the comments object
+    if(comments[entry.data.path]){
+      comments[entry.data.path].push(comment);
+    } else {
+      comments[entry.data.path] = [comment];
+    }
 
-//   });
+  });
 
-//   return comments;
+  return comments;
 
-// };
+};
+
+
+// const fs = require("fs");
+// const path = require("path");
+
+// let comments = [];
+
+// try {
+//   const filePath = path.join(__dirname, "approved-comments_submissions.json");
+
+//   if (fs.existsSync(filePath)) {
+//     comments = require(filePath);
+//   } else {
+//     console.warn("🟡 No approved-comments_submissions.json file found.");
+//   }
+// } catch (err) {
+//   console.error("🔴 Failed to load approved comments data:", err);
+// }
+
+// module.exports = comments;
--- src/functions/comment-handler.js
+++ src/functions/comment-handler.js
@@ -1,72 +1,142 @@
-'use strict';
-
-var request = require("request");
-
-// populate environment variables locally.
+// Updated function with environment variable for Slack webhook
 require('dotenv').config();
 
-const URL = "https://awc-dev-mode.netlify.app";
+const request = require('request');
 
-/*
-  Our serverless function handler
-*/
+const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL; // Get this from the env variable
+
 export function handler(event, context, callback) {
+  const body = JSON.parse(event.body);
 
-  // get the arguments from the notification
-  var body = JSON.parse(event.body);
-
-  // prepare call to the Slack API
-  var slackURL = process.env.SLACK_WEBHOOK_URL
-
-  ;  // Ensure the environment variable is correctly named
-  var slackPayload = {
-    "text": "New comment on " + URL,
-    "attachments": [
+  const slackPayload = {
+    text: `New comment on the site: ${body.data.path}`,
+    attachments: [
       {
-        "fallback": "New comment on the site",
-        "color": "#444",
-        "author_name": body.data.email,
-        "title": body.data.path,
-        "title_link": URL + body.data.path,
-        "text": body.data.comment
+        fallback: "New comment on the site",
+        color: "#444",
+        author_name: body.data.email,
+        title: body.data.path,
+        title_link: `https://your-site-url.com${body.data.path}`,
+        text: body.data.comment,
       },
       {
-        "fallback": "Manage comments on " + URL,
-        "callback_id": "comment-action",
-        "actions": [
+        fallback: "Manage comments on the site",
+        callback_id: "comment-action",
+        actions: [
           {
-            "type": "button",
-            "text": "Approve comment",
-            "name": "approve",
-            "value": body.id
+            type: "button",
+            text: "Approve comment",
+            name: "approve",
+            value: body.id,
           },
           {
-            "type": "button",
-            "style": "danger",
-            "text": "Delete comment",
-            "name": "delete",
-            "value": body.id
-          }
-        ]
-      }
-    ]
+            type: "button",
+            style: "danger",
+            text: "Delete comment",
+            name: "delete",
+            value: body.id,
+          },
+        ],
+      },
+    ],
   };
 
-  // post the notification to Slack
-  request.post({ url: slackURL, json: slackPayload }, function(err, httpResponse, body) {
-    let msg;
-    if (err) {
-      msg = 'Post to Slack failed: ' + err;
-      console.error(msg);
-    } else {
-      msg = 'Post to Slack successful! Server responded with status code: ' + httpResponse.statusCode + ' and body: ' + JSON.stringify(body);
-      console.log(msg);
+  // Post the notification to Slack
+  request.post(
+    {
+      url: SLACK_WEBHOOK_URL,
+      json: slackPayload,
+    },
+    function (err, httpResponse, body) {
+      if (err) {
+        console.error('Failed to post to Slack:', err);
+        callback(null, {
+          statusCode: 500,
+          body: 'Failed to send Slack notification',
+        });
+      } else {
+        console.log('Posted to Slack successfully!');
+        callback(null, {
+          statusCode: 200,
+          body: 'Notification sent to Slack!',
+        });
+      }
     }
-
-    callback(null, {
-      statusCode: 200,
-      body: msg
-    });
-  });
+  );
 }
+
+
+
+
+// 'use strict';
+
+// var request = require("request");
+
+// // populate environment variables locally.
+// require('dotenv').config();
+
+// const URL = "https://awc-dev-mode.netlify.app";
+
+// /*
+//   Our serverless function handler
+// */
+// export function handler(event, context, callback) {
+
+//   // get the arguments from the notification
+//   var body = JSON.parse(event.body);
+
+//   // prepare call to the Slack API
+//   var slackURL = process.env.SLACK_WEBHOOK_URL
+
+//   ;  // Ensure the environment variable is correctly named
+//   var slackPayload = {
+//     "text": "New comment on " + URL,
+//     "attachments": [
+//       {
+//         "fallback": "New comment on the site",
+//         "color": "#444",
+//         "author_name": body.data.email,
+//         "title": body.data.path,
+//         "title_link": URL + body.data.path,
+//         "text": body.data.comment
+//       },
+//       {
+//         "fallback": "Manage comments on " + URL,
+//         "callback_id": "comment-action",
+//         "actions": [
+//           {
+//             "type": "button",
+//             "text": "Approve comment",
+//             "name": "approve",
+//             "value": body.id
+//           },
+//           {
+//             "type": "button",
+//             "style": "danger",
+//             "text": "Delete comment",
+//             "name": "delete",
+//             "value": body.id
+//           }
+//         ]
+//       }
+//     ]
+//   };
+
+//   // post the notification to Slack
+//   request.post({ url: slackURL, json: slackPayload }, function(err, httpResponse, body) {
+//     let msg;
+//     if (err) {
+//       msg = 'Post to Slack failed: ' + err;
+//       console.error(msg);
+//     } else {
+//       msg = 'Post to Slack successful! Server responded with status code: ' + httpResponse.statusCode + ' and body: ' + JSON.stringify(body);
+//       console.log(msg);
+//     }
+
+//     callback(null, {
+//       statusCode: 200,
+//       body: msg
+//     });
+//   });
+// }
 
